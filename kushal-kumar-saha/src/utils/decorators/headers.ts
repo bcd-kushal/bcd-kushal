1f67ddf8e34ad138854628527f43d61e8dad41a6
Kushal <dev@kushalkumarsaha.com>
2024-03-14 23:53:34 +0530

    Add files via upload

--- kushal-kumar-saha/src/utils/decorators/headers.ts
+++ kushal-kumar-saha/src/utils/decorators/headers.ts
@@ -3,3 +3,5 @@
 export const HEADER_H1          = "font-medium text-[80px] leading-[80px] tracking-[-1.6px]"
 export const HEADER_H2          = "font-normal text-[31px] leading-[35.3px]"
 export const HEADER_H3          = "font-normal text-[22px] leading-[28.6px]"
+
+export const HEADER_H5          = "font-normal text-[15px] leading-[16.9px]"

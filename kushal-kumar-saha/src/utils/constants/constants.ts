04a31859d8e29cd751f4452c795e4922fa956df0
Kushal <dev@kushalkumarsaha.com>
2024-03-14 23:54:08 +0530

    Add files via upload

---
+++ kushal-kumar-saha/src/utils/constants/constants.ts
@@ -0,0 +1,4 @@
+
+/* HEADER -----------------------> */
+export const HEADER_TOOLTIP_DELAY_DURATION =        280        // ms
+export const HEADER_TOOLTIP_CONTENT_OFFSET =        22         // px
--- kushal-kumar-saha/src/utils/types/types.ts
+++ kushal-kumar-saha/src/utils/types/types.ts
@@ -1,5 +1,11 @@
+import { headerDataProps } from "@/components/blogs/header/headerComponents"
 import React from "react"
 
 export type ClassNameType               = string 
-export type ThemeType                   = 'light' | 'dark'
 export type ChildrenType                = Readonly<React.ReactNode>
+
+export type ThemeType                   = 'light' | 'dark'
+export type HeaderLinkTypes             = keyof headerDataProps
+
+
+

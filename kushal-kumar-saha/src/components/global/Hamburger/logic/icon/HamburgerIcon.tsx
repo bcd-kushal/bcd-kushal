1bea8576c1dc9ff25b1b9b76c19ea9cfa30bb76a
Kushal <dev@kushalkumarsaha.com>
2024-05-24 23:53:03 +0530

    Delete kushal-kumar-saha/src/components/global/Hamburger directory

--- kushal-kumar-saha/src/components/global/Hamburger/logic/icon/HamburgerIcon.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export function HamburgerIcon({ topBarId, bottomBarId }: { topBarId: string, bottomBarId: string }) {
-    return (
-        <div className="grid *:row-start-1 *:col-start-1 *:h-[1.3px] *:w-4 *:bg-slate-200">
-            <span id={topBarId} className="-translate-y-1"></span><span id={bottomBarId} className="translate-y-1"></span>
-        </div>
-    )
-}

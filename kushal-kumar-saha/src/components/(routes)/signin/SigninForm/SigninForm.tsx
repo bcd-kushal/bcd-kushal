a4c0ff124aa8dec0836ac72a5f9c757a6107065e
Kushal <dev@kushalkumarsaha.com>
2024-05-24 23:53:51 +0530

    Add files via upload

--- kushal-kumar-saha/src/components/(routes)/signin/SigninForm/SigninForm.tsx
+++ kushal-kumar-saha/src/components/(routes)/signin/SigninForm/SigninForm.tsx
@@ -16,7 +16,7 @@ export default function SigninForm({ className }:{ className?:ClassNameType }) {
         console.table({user:username,pass:password})
     }
     return (
-        <form action={handleFormSubmit} className={className}>
+        <form action={handleFormSubmit} autoComplete={"off"} className={className}>
             <InputForm type="text"      name={usernameName} placeholder="Username" className={`${formInputStyles}`} setParentState={setUsernameEmpty} />
             <InputForm type="password"  name={passwordName} placeholder="Password" className={`${formInputStyles}`} setParentState={setPasswordEmpty} />
             <button type="submit" className={`absolute bottom-[6px] left-1/2 -translate-x-1/2 md:left-0 md:translate-x-0 p-3 md:pl-0 md:bottom-4 text-3xl text-[#808af250] transition-all duration-300 hover:text-[#4e4eda]  ${passwordEmpty || usernameEmpty ? "opacity-0 translate-y-[100dvh]" : "-translate-y-0 opacity-100"}`} onClick={()=>setSubmitClicked(prev=>true)} disabled={submitClicked}>Sign in</button>
@@ -37,7 +37,7 @@ const InputForm = ({ type, name, placeholder, setParentState, className }:{ type
     }, [emptyField])
     return (
         <div className="relative group grid *:row-start-1 *:col-start-1 min-w-[280px]">
-            <input type={type} name={name} onChange={(e)=>setEmptyField(prev=>e.target.value ? false : true)} className={`peer z-20 autofill:bg-[#0000] autofill:text-white text-lg ${formInputStyles}`} />
+            <input type={type} autoComplete="off" name={name} onChange={(e)=>setEmptyField(prev=>e.target.value ? false : true)} className={`peer z-20 autofill:bg-[#0000] autofill:text-white text-lg ${formInputStyles}`} />
             <span className="peer-focus:w-2 peer-focus:-bottom-[1px] md:peer-focus:-bottom-[2px] aspect-square bg-white w-1 absolute bottom-[0.5px] md:-bottom-[0.5px] input-square transition-all duration-150"></span>
             <span className="input-line-dash-cover absolute bottom-[2px] md:bottom-[1px] left-0 w-0 h-[1px] bg-[#777] z-10"></span>
             <span className="absolute bottom-[2px] md:bottom-[1px] left-0 h-[1px] bg-gradient-to-r from-white/50 to-transparent transition-all duration-300 border-span-animate"></span>

83c7cf0ec81f8c8ed7930c9ab30545523452851e
Kushal <dev@kushalkumarsaha.com>
2024-05-24 23:49:59 +0530

    Add files via upload

--- kushal-kumar-saha/src/components/shadcn/carousel.tsx
+++ kushal-kumar-saha/src/components/shadcn/carousel.tsx
@@ -2,6 +2,7 @@ import { useRef } from "react"
 import Autoplay from "embla-carousel-autoplay"
 import { Card, CardContent } from "@/components/ui/card"
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, } from "@/components/ui/carousel"
+import Image from "next/image"
 
 export type CaraouselImagesType = {src:string,alt:string}[]
 interface CarouselProps { images:CaraouselImagesType, width:string, slideAfter:number, aspect?:string, hideLeft?:boolean, hideRight?:boolean }
@@ -15,7 +16,7 @@ export function CarouselComponent({images,width,aspect,slideAfter,hideLeft,hideR
                     <CarouselItem key={index}>
                         <Card className={`border-[0.6667px] border-[#32449058] max-w-[${width}] overflow-hidden`}>
                             <CardContent className={`flex aspect-[${aspect||"16/9"}] items-center justify-center p-6 overflow-hidden rounded-[6px]`}>
-                                <img className="scale-[1.22]" src={image.src} alt={image.alt} />
+                                <Image className="scale-[1.22]" src={image.src} alt={image.alt} width={282} height={180} />
                             </CardContent>
                         </Card>
                     </CarouselItem>
